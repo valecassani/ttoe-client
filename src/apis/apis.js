2569ee3b2d7d0d16c55890ae886b0cdb6ad2ac28
Valerio Cassani <valeriocassani@MacBook-Pro-di-Valerio.local>
2019-12-02 23:09:54 +0100

    added production url

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -1,5 +1,5 @@
-// const baseUrl = "https://ttoe.herokuapp.com";
-const baseUrl = "http://localhost:3000";
+const baseUrl = 'https://ttoe.herokuapp.com';
+// const baseUrl = "http://localhost:3000";
 
 export const getGame = async id => {
     const resp = await fetch(`${baseUrl}/games/${id}`);
@@ -8,29 +8,29 @@ export const getGame = async id => {
 
 export const resetGame = async id => {
     const resp = await fetch(`${baseUrl}/games/${id}/reset`, {
-        method: "POST",
+        method: 'POST'
     });
     return await resp.json();
 };
 
 export const performMove = async (xPos, yPos, user, gameId) => {
     const resp = await fetch(`${baseUrl}/games/${gameId}/move`, {
-        method: "POST",
+        method: 'POST',
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ xPos, yPos, user }),
+        body: JSON.stringify({ xPos, yPos, user })
     });
     return await resp.json();
 };
 
 export const createGame = async ({ players, dimension }) => {
     const resp = await fetch(`${baseUrl}/games`, {
-        method: "POST",
+        method: 'POST',
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ dimension, players }),
+        body: JSON.stringify({ dimension, players })
     });
     return await resp.json();
 };
