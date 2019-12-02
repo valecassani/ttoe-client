c2dac9d7923d1e5aaca29e1813d0a178791d6634
Valerio Cassani <valerio.cassani@eidosmedia.com>
2019-12-02 16:14:56 +0100

    updated prettier

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import {  Switch, Route } from 'react-router';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { Switch, Route } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 import Game from './views/Game';
 import Config from './views/Config';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-        <Switch>
-          <Route path="/:gameId" render={props => <Game {...props} />} >
-          </Route>
-          <Route path="/">
-            <Config />
-          </Route>
-        </Switch>
-        </Router>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+            <Router>
+                <Switch>
+                    <Route path="/:gameId" render={props => <Game {...props} />}></Route>
+                    <Route path="/">
+                        <Config />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/views/Config/Config.js
+++ src/views/Config/Config.js
@@ -1,36 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { ToggleButtonGroup, ToggleButton } from '@material-ui/lab';
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-import { withRouter } from "react-router";
+import { withRouter } from 'react-router';
 
 const useStyles = makeStyles({
     container: {
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'column'
     },
     innerGroup: {
         margin: '20px'
     }
-})
+});
 
 const createGame = ({ players, dimension, history }) => {
-    fetch('https://ttoe.herokuapp.com/games', { 
-        method: 'POST', 
+    fetch('https://ttoe.herokuapp.com/games', {
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ dimension, players })
-    }).then(resp => resp.json())
-    .then(({ id }) => {
-        console.log('created id', id);
-        history.push(`/${id}`)
-
-    }).catch(
-        err => console.error(err)
-    )
-}
+    })
+        .then(resp => resp.json())
+        .then(({ id }) => {
+            console.log('created id', id);
+            history.push(`/${id}`);
+        })
+        .catch(err => console.error(err));
+};
 
 const Config = ({ history }) => {
     const [players, setPlayers] = useState(2);
@@ -38,27 +37,26 @@ const Config = ({ history }) => {
 
     const classes = useStyles();
     return (
-        <div className={classes.container} >
-            <div>
-                Config
-            </div>
+        <div className={classes.container}>
+            <div>Config</div>
             <div className={classes.innerGroup}>
                 <div>
                     <span>Select Players number</span>
                 </div>
-                <ToggleButtonGroup 
+                <ToggleButtonGroup
                     value={players}
                     exclusive
                     onChange={e => setPlayers(parseInt(e.currentTarget.value))}
-                    aria-label="Players" >
+                    aria-label="Players"
+                >
                     <ToggleButton value={2} aria-label="2">
-                        <span>Two Players</span>    
+                        <span>Two Players</span>
                     </ToggleButton>
                     <ToggleButton value={3} aria-label="3">
-                        <span>Three Players</span>    
+                        <span>Three Players</span>
                     </ToggleButton>
                     <ToggleButton value={4} aria-label="4">
-                        <span>Four Players</span>    
+                        <span>Four Players</span>
                     </ToggleButton>
                 </ToggleButtonGroup>
             </div>
@@ -66,30 +64,28 @@ const Config = ({ history }) => {
                 <div>
                     <span>Select Grid dimensions</span>
                 </div>
-                <ToggleButtonGroup 
+                <ToggleButtonGroup
                     value={dimension}
                     exclusive
                     onChange={e => setDimension(parseInt(e.currentTarget.value))}
-                    aria-label="Size">
-
+                    aria-label="Size"
+                >
                     <ToggleButton value={3} aria-label="3 x 3">
-                        <span>3 x 3</span>    
+                        <span>3 x 3</span>
                     </ToggleButton>
                     <ToggleButton value={5} aria-label="5 x 5">
-                        <span>5 x 5</span>    
+                        <span>5 x 5</span>
                     </ToggleButton>
                     <ToggleButton value={10} aria-label="10 x 10">
-                        <span>10 x 10</span>    
+                        <span>10 x 10</span>
                     </ToggleButton>
-                    
                 </ToggleButtonGroup>
             </div>
-            <Button onClick={() => createGame({ dimension, players, history })} >
+            <Button onClick={() => createGame({ dimension, players, history })}>
                 <span>Create Game</span>
             </Button>
-
         </div>
-    )
-}
+    );
+};
 
 export default withRouter(Config);
